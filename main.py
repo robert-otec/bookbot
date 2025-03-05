5ee6296c3c268fe35db2290fef84db3859a181ec
robert-otec <robertotec@outlook.cz>
2025-03-05 19:49:04 +0100

    update readme with a description

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import sys
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
