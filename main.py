d702d556055223aa851f0943026ff756c0d3d61c
robert-otec <robertotec@outlook.cz>
2025-09-26 11:46:24 +0200

    import sys to avoid hard coding path to books

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
+import sys
 
 
 def main():
-    filepath = "books/frankenstein.txt"
-    text = get_book_text(filepath)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print_report(filepath, num_words, chars_sorted_list)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+        text = get_book_text(filepath)
+        num_words = get_num_words(text)
+        chars_dict = get_chars_dict(text)
+        chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+        print_report(filepath, num_words, chars_sorted_list)
 
 
 
